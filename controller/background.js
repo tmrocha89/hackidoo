896e23497e85b00bd0ff6e0716537ffa6e82bfc8
Tiago Rocha <tmrocha89@gmail.com>
2017-01-10 19:11:15 +0000

    SRP - CollectionRepository

--- controller/background.js
+++ controller/background.js
@@ -2,6 +2,10 @@ console.log("Background is running");
 var self = this;
 var token = null;
 
+var userRepo = new UserRepository();
+
+var collectionRepo = null;// new CollectionRepository(self.token);
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.action == "getCollections"){
@@ -55,58 +59,7 @@ function UserRepository(){
     }
 }
 
-var userRepo = new UserRepository();
 
-function CollectionRepository(){
-    var self = this;
-    var request = new Request();
-
-    this._getCollectionsJson = function(data, cb){
-        if(!data){
-            data = {};
-        }
-        if(!data.collections){
-            data.collections = [];
-        }
-        userRepo.getCollectionsUrl(function(url){
-            request.get(url, true, {"Authorization": "Bearer "+token}, function(textList){
-                var response = JSON.parse(textList);
-                console.log("Collections List");
-                if(response){
-                    
-                    if(response.content){
-                        var contentList = response.content.map(function(collection){
-                            return new Collection(collection);
-                        });
-                        data.collections = data.collections.concat(contentList[0]);
-                    }
-                }
-                var pageInfo = response.page;
-                if(pageInfo.number < pageInfo.totalPages){
-                   //request all pages
-                }
-                console.log(data.collections);
-                cb(data.collections);
-            });
-        });
-
-        //userRepo.getUser(data.token, function(){
-//
-//        });
-        /*var xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://cookidoo.pt/vorwerkWebapp/app#/collection/8796125921281", true);
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == 4) {
-            }
-        };*/
-    };
-
-    this.getAllCollections = function(cb){
-        this._getCollectionsJson(null, cb);
-    }
-}
-
-var collectionRepo = new CollectionRepository();
 
 function RecipeRepository(){ // Work in Progress
     var self = this;
@@ -222,6 +175,7 @@ function Download(){
     var self = this;
 
     this.collections = function(callback){
+        collectionRepo = new CollectionRepository(token);
         collectionRepo.getAllCollections(function(collections){
             console.log("::Collections::"); console.log(collections);
             console.log("Now its time to download recipes :)");
---
+++ repository/CollectionRepository.js
@@ -0,0 +1,49 @@
+function CollectionRepository(token){
+    var self = this;
+    var token = token;
+    var request = new Request();
+
+    this._getCollectionsJson = function(data, cb){
+        if(!data){
+            data = {};
+        }
+        if(!data.collections){
+            data.collections = [];
+        }
+        userRepo.getCollectionsUrl(function(url){
+            request.get(url, true, {"Authorization": "Bearer "+this.token}, function(textList){
+                var response = JSON.parse(textList);
+                console.log("Collections List"); console.log(response);
+                if(response){
+                    
+                    if(response.content){
+                        var contentList = response.content.map(function(collection){
+                            return new Collection(collection);
+                        });
+                        data.collections = data.collections.concat(contentList[0]);
+                    }
+                }
+                var pageInfo = response.page;
+                if(pageInfo.number < pageInfo.totalPages){
+                   //request all pages
+                }
+                console.log(data.collections);
+                cb(data.collections);
+            });
+        });
+
+        //userRepo.getUser(data.token, function(){
+//
+//        });
+        /*var xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://cookidoo.pt/vorwerkWebapp/app#/collection/8796125921281", true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+            }
+        };*/
+    };
+
+    this.getAllCollections = function(cb){
+        this._getCollectionsJson(null, cb);
+    }
+}
