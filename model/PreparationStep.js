bf7069af92e65a79b240d10a69988446e3dd5e35
Tiago Rocha <tmrocha89@gmail.com>
2017-01-13 18:50:01 +0000

    PreparationStep storing only it needs

--- model/PreparationStep.js
+++ model/PreparationStep.js
@@ -1,16 +1,32 @@
 function PreparationStep(preparationArrayElem){
     var self = this;
-    this._data = preparationArrayElem;
+    this._number = 0;
+    this._text = "";
+
+    var setupPreparationStep = function(step){
+        self._setNumber(step.number);
+        self._setText(step.formattedText);
+    };
+
+    this._setNumber = function(num){
+        self._number = num;
+    };
+
+    this._setText = function(text){
+        self._text = text;
+    };
 
     this.getStepPosition = function(){
-        return self._data.number;
+        return self._number;
     ;}
 
     this.getText = function(){
-        return self._data.formattedText;
+        return self._text;
     };
 
     this.toString = function(){
         return self.getStepPosition() + ")  " + self.getText();
     };
+
+    setupPreparationStep(preparationArrayElem);
 }
