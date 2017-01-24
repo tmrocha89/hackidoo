58e9fefd523dcfdd5897e6149e37a2f842feae0f
Tiago Rocha <tmrocha89@gmail.com>
2017-01-24 18:39:59 +0000

    post request implemented. post and get now are using a private method _request

--- network/Request.js
+++ network/Request.js
@@ -1,20 +1,32 @@
-function Request(){
+function Request() {
     var self = this;
+    this._HTTP_METHODS = {
+        POST: "POST",
+        GET: "GET"
+    };
 
-    this.get = function(url, async, headers, cb){
+    this._request = function(method, url, async, headers, data, cb) {
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", url, async);
-        if(headers){
-            for(var key in headers){
+        xhr.open(method, url, async);
+        if (headers) {
+            for (var key in headers) {
                 xhr.setRequestHeader(key, headers[key]);
             }
         }
-        
+
         xhr.onreadystatechange = function() {
             if (xhr.readyState == 4) {
                 cb(xhr.responseText);
             }
         };
-        xhr.send();
-    }
+        xhr.send(data);
+    };
+
+    this.get = function(url, async, headers, cb) {
+        return self._request(self._HTTP_METHODS.GET, url, async, headers, null, cb);
+    };
+
+    this.post = function(url, async, headers, data, cb) {
+        return self._request(self._HTTP_METHODS.POST, url, async, headers, data, cb);
+    };
 }
