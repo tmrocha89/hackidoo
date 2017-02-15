113ed50e807209b07a1e01258de660db2d656697
Tiago Rocha <tmrocha89@gmail.com>
2017-02-15 22:56:53 +0000

    improved logic to check the url and call the corresponding method

--- controller/popup.js
+++ controller/popup.js
@@ -9,66 +9,70 @@
  *   is found.
  */
 function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
+    // Query filter to be passed to chrome.tabs.query - see
+    // https://developer.chrome.com/extensions/tabs#method-query
+    var queryInfo = {
+        active: true,
+        currentWindow: true
+    };
 
-  chrome.tabs.query(queryInfo, function(tabs) {
-    var tab = tabs[0];
+    chrome.tabs.query(queryInfo, function(tabs) {
+        var tab = tabs[0];
 
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
+        // A tab is a plain object that provides information about the tab.
+        // See https://developer.chrome.com/extensions/tabs#type-Tab
+        var url = tab.url;
 
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
+        // tab.url is only available if the "activeTab" permission is declared.
+        // If you want to see the URL of other tabs (e.g. after removing active:true
+        // from |queryInfo|), then the "tabs" permission is required to see their
+        // "url" properties.
+        console.assert(typeof url == 'string', 'tab.url should be a string');
 
-    callback(url);
-  });
+        callback(url);
+    });
 }
 
-var isViewingAllCollections = function(url){
-    var cookidooRegEx = /^http(s)?:\/\/cookidoo.pt\/vorwerkWebapp\/app#\/mytreasure\/collections/i;
-    return url.match(cookidooRegEx);
-};
+var self = this;
 
-var isViewingAllRecipes = function(url){
-    //ToDo: Change me
-    var cookidooRegEx = /^http(s)?:\/\/cookidoo.pt\/vorwerkWebapp\/app#\/collection\/[0-9]+$/i;
-    return url.match(cookidooRegEx);
-};
+this._URL_DATA = [{
+    urlRegEx: /^http(s)?:\/\/cookidoo.pt\/vorwerkWebapp\/app#\/mytreasure\/collections/i,
+    type: "Collections",
+    url: ""
+}, {
+    urlRegEx: /^http(s)?:\/\/cookidoo.pt\/vorwerkWebapp\/app#\/collection\/[0-9]+$/i,
+    type: "Recipes",
+    url: ""
+}, {
+    urlRegEx: /^http(s)?:\/\/cookidoo.pt\/vorwerkWebapp\/app#\/collection\/[0-9]+\/recipe\/[0-9]+/i,
+    type: "Recipe",
+    url: ""
+}];
 
-var isViewingARecioe = function(url){
-    //ToDo: Change me
-    var cookidooRegEx = /^http(s)?:\/\/cookidoo.pt\/vorwerkWebapp\/app#\/collection\/[0-9]+\/recipe\/[0-9]+/i;
-    return url.match(cookidooRegEx);
-};
-
-var getActualViewingPage = function(url){
-        if(isViewingAllCollections(url)){
-            return "All Collections";
+var getActualViewingPage = function(url) {
+    for (var i = 0; i < self._URL_DATA.length; i++) {
+        var urlData = self._URL_DATA[i];
+        if (url.match(urlData.urlRegEx)) {
+            return urlData;
         }
-        if(isViewingAllRecipes(url)){
-            return "All Recipes";
-        }
-        if(isViewingARecioe(url)){
-            return "A Recipe";
-        }
-        return "Where are your?";
+    }
+    return null;
 };
 
-getCurrentTabUrl(function(url){
+var notifyScripts = function(callback) {
+    console.log("Calling scripts");
+    var cb = callback ? callback : function() {};
+    return cb;
+};
+
+getCurrentTabUrl(function(url) {
     console.log(url);
-    var cookidooRegEx = /^http(s)?:\/\/cookidoo.pt\/vorwerkWebapp\/app#\/mytreasure\/collections/i;
-
-    var viewing = getActualViewingPage(url);
-    $("#status").text(viewing);
+    var urlObject = getActualViewingPage(url);
+    var textToShow = "UNKNOWN";
+    if (urlObject) {
+        textToShow = urlObject.type;
+        notifyScripts({ action: "download", url: urlObject.url, type: urlObject.type });
+    }
+    $("#status").text(textToShow);
 
 });
-
